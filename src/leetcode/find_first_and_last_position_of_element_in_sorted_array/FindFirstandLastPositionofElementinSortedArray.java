7fbfac619420a871dcff39d7fcfc35126830e7bc
Björklin, Nils /External <nils.bjorklin@teliacompany.com>
2019-03-04 09:36:25 +0100

    Find First and Last Position of Element in Sorted Array

---
+++ src/leetcode/find_first_and_last_position_of_element_in_sorted_array/FindFirstandLastPositionofElementinSortedArray.java
@@ -0,0 +1,52 @@
+package leetcode.find_first_and_last_position_of_element_in_sorted_array;
+
+public class FindFirstandLastPositionofElementinSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        int endOfArray = nums.length - 1;
+        if (endOfArray == -1 || nums[0] > target || nums[endOfArray] < target)
+            return new int[] { -1, -1 };
+        int firstInstance = findFirst(nums, 0, endOfArray, target);
+        if (firstInstance == -1)
+            return new int[] { -1, -1 };
+        return new int[] { firstInstance, findLast(nums, firstInstance, endOfArray, target) };
+    }
+
+    private int findFirst(int[] nums, int start, int end, int target) {
+        if (start > end)
+            return -1;
+        int pivot = start + (end - start) / 2;
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if (i == pivot)
+                sb.append("!");
+            sb.append(nums[i]);
+            if(i != end)
+            sb.append(", ");
+        }
+        String debug = sb.toString();
+        if (nums[start] == target)
+            return start;
+        if (nums[pivot] == target && (pivot == 0 || nums[pivot - 1] != target))
+            return pivot;
+        if (nums[pivot] < target) {
+            return findFirst(nums, pivot + 1, end, target);
+        } else if (nums[pivot] >= target) {
+            return findFirst(nums, start, pivot - 1, target);
+        }
+        return -1;
+    }
+
+    private int findLast(int[] nums, int start, int end, int target) {
+        int pivot = start + (end - start) / 2;
+        if (nums[end] == target)
+            return end;
+        if (nums[pivot] == target && nums[pivot + 1] != target)
+            return pivot;
+        if (nums[pivot] <= target) {
+            return findLast(nums, pivot + 1, end, target);
+        } else if (nums[pivot] > target) {
+            return findLast(nums, start, pivot - 1, target);
+        }
+        return -1;
+    }
+}
