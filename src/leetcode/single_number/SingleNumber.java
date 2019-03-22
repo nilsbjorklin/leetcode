33f6b831e57c9d62c25cb244d0d06d581b2ca443
Björklin, Nils /External <nils.bjorklin@teliacompany.com>
2019-03-22 11:50:07 +0100

    Single Number

---
+++ src/leetcode/single_number/SingleNumber.java
@@ -0,0 +1,14 @@
+package leetcode.single_number;
+
+import java.util.Arrays;
+
+public class SingleNumber {
+    int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] != nums[i + 1])
+                return nums[i];
+        }
+        return nums[nums.length - 1];
+    }
+}
